0ee0736b06c63ed4f5449067bc6d5d11314de31f
Zuha Maryam <zuhamaryam676@gmail.com>
2026-05-23 19:19:02 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'blobs.vusercontent.net',
+      },
+    ],
+  },
+}
+
+export default nextConfig
